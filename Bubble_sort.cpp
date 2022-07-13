0e67ee649269f33f528a871bc9db841e72651221
Hariom Rathore <hariomrathore19@gmail.com>
2022-07-13 11:09:34 +0530

    Update Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -13,15 +13,18 @@ int main(){
     }
     // Method for Bubble sort.
 
-    for(int i=0 ; i<n-1 ; i++){
-        for(int j=i+1; j<=n-i ;j++){
-            if(arr[j]<arr[i]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+    int counter = 1;
+    while (counter < n){
+        for(int i=0 ; i<n-counter ; i++){
+            if(arr[i]>arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1]; 
+                arr[i+1] = temp;
             }
         }
+        counter++;
     }
+    
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
